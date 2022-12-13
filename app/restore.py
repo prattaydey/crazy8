b64adb44a6676cc282082d10cb0010527caf7299
KevinLi0711 <kli30@stuy.edu>
2022-12-12 21:58:49 -0500

    main page update

--- app/restore.py
+++ app/restore.py
@@ -3,9 +3,10 @@ import json
 blobId = 1051631725620510720
 
 def restore():
-    data = {"not_a_real_id" : "not_a_real_room"}
+    data = {"not_a_real_id" : {"room_name" : "not_a_real_room", "player1" : "not_a_real_player", "player2" : "not_a_real_player"}}
     data = json.dumps(data)
     url = f"https://jsonblob.com/api/room/{blobId}"
     requests.put(url, data=data)
+    print(url)
 
 restore()
